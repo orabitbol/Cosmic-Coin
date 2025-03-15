48865514a3726cc329359fc5f0eb603e96b264e2
Or Abitbol <orabitbol@Ors-MacBook-Pro.local>
2025-03-15 20:03:15 +0200

    add config from versel

--- src/firebase.ts
+++ src/firebase.ts
@@ -13,14 +13,13 @@ import {
   
 
   const firebaseConfig = {
-    apiKey: "AIzaSyCxFHPsNSfgWag0Vgfkb_mWFi-cc2xMNZY",
-    authDomain: "cosmic-coin-5d8a5.firebaseapp.com",
-    projectId: "cosmic-coin-5d8a5",
-    storageBucket: "cosmic-coin-5d8a5.appspot.com",
-    messagingSenderId: "655504154896",
-    appId: "1:655504154896:web:ffe4403890784721f2c595"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
   };
-  
   // 🔹 אתחול Firebase
   const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
