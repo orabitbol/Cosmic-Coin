d43fa25af4a79f452ea26a7833a723ac998d4bf6
Or Abitbol <orabitbol@Ors-MacBook-Pro.local>
2025-03-16 00:30:42 +0200

    fix the .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,14 +14,13 @@ if (!import.meta.env.VITE_FIREBASE_API_KEY) {
   throw new Error("🚨 Missing Firebase API Key!");
 }
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
 console.log("🔍 Checking Firebase env variables...");
 console.log("🔥 API Key:", import.meta.env.NEXT_PUBLIC_FIREBASE_API_KEY || "🚨 MISSING!");
 console.log("🔥 Auth Domain:", import.meta.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "🚨 MISSING!");
